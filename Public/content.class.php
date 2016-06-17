60c432e8ffb417736d120f69c46b922349dd7054
Slembit <erik.lovenstad@gmail.com>
2016-06-17 15:45:52 +0200

    content.class.php added.

---
+++ Public/content.class.php
@@ -0,0 +1,25 @@
+<?php
+
+class Content{
+
+	public static function skills($token){
+		$curl = new Curl();
+
+		$curl->get("http://www.therewillbecode.se/slick2/?/skills/all/loadskills/&token=$token");
+		// var_dump($curl->response);
+		$response = json_decode($curl->response, true);		
+		return $response;
+
+
+	}
+
+	public static function users($token){
+		$curl = new Curl();
+
+		$curl->get("http://www.therewillbecode.se/slick2/?/skills/all/loadskills/&token=$token");
+		// var_dump($curl->response);
+		$response = json_decode($curl->response, true);		
+		return $response;
+	}
+
+}
