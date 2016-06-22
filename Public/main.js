75f4f5b5e6079cc8e8d8e3cc53f5490df2744354
Slembit <erik.lovenstad@gmail.com>
2016-06-22 19:26:25 +0200

    removed 2 files updated main js

--- Public/main.js
+++ Public/main.js
@@ -231,7 +231,7 @@ console.log("http://www.therewillbecode.se/slick2/?/persons/"+id+"/loadnextprofi
 
 	    		$( slideItem ).html("<div class='profileContent'><div class='profileContentInner'><label class='profileName'><a href='single.php?id="+item.id+"'> "
 	    			+item.firstname+" "+item.lastname+
-	    			"</a> </label><img class='profilePicture' src='https://organicthemes.com/demo/profile/files/2012/12/profile_img.png'><span class='profileSkills'><br><u>SKILLS:</u><ul><li>"+item.skills+"</li></ul></span><div class='profileShortDesc'><label><u>Description:</u></label>"+item.lookingfor+"</div></div></div>");
+	    			"</a> </label><span class='profileSkills'><br><u>SKILLS:</u><ul><li>"+item.skills+"</li></ul></span><div class='profileShortDesc'>"+item.lookingfor+"</div></div></div>");
 	    			
 				slideindex++;
 
@@ -260,7 +260,7 @@ function getProfilesStart(initUrl){
 
 	    		$( slideItem ).html("<div class='profileContent'><div class='profileContentInner'><label class='profileName'><a href='single.php?id="+item.id+"'> "
 	    			+item.firstname+" "+item.lastname+
-	    			"</a> </label><img class='profilePicture' src='https://organicthemes.com/demo/profile/files/2012/12/profile_img.png'><span class='profileSkills'><br><u>SKILLS:</u><ul><li>"+item.skills+"</li></ul></span><div class='profileShortDesc'><label><u>Description:</u></label>"+item.lookingfor+"</div></div></div>");
+	    			"</a> </label><span class='profileSkills'><br><u>SKILLS:</u><ul><li>"+item.skills+"</li></ul></span><div class='profileShortDesc'>"+item.lookingfor+"</div></div></div>");
 	    			//.css("background-image","url("+item.profilepicture+")")
 					slideindex++;
 
@@ -286,7 +286,7 @@ function getSingle(id){
  	    		theID = item.id;
  	    		$( "#singleCv" ).html("<div class='profileContent'><div class='profileContentInner'><label class='profileName'><a href='single.php?id="+item.id+"'> "
 	    			+item.firstname+" "+item.lastname+
-	    			"</a> </label><img class='profilePicture' src='https://organicthemes.com/demo/profile/files/2012/12/profile_img.png'><span class='profileSkills'><br><u>SKILLS:</u><ul><li>PHP</li><li>JavaScript</li><li>CSS</li></ul></span><div class='profileShortDesc'><label><u>Description:</u></label>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div><span class='menuLogo'><a href=''>Contact</a></span> </div></div>").css("background-image","linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)),url("+item.profilepicture+")");
+	    			"</a> </label><span class='profileSkills'><br><u>SKILLS:</u><ul><li>"+item.skills+"</li></ul></span><div class='profileShortDesc'><label></label>"+item.about+"</div><span class='menuLogo'><a href=''>Contact</a></span> </div></div>").css("background-image","linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)),url("+item.profilepicture+")");
 	    			});
  	     },
  		error: function(){
@@ -301,32 +301,3 @@ function errorHndl(request, status, error) {
     console.log(request.responseText, status.responseText, error.responseText);
 
 }
-
-///admin page
-
-$(document).ready(function() {
-$("#register").click(function() {
-var name = $("#name").val();
-var email = $("#email").val();
-var password = $("#password").val();
-var cpassword = $("#cpassword").val();
-if (name == '' || email == '' || password == '' || cpassword == '') {
-alert("Please fill all fields...!!!!!!");
-} else if ((password.length) < 8) {
-alert("Password should atleast 8 character in length...!!!!!!");
-} else if (!(password).match(cpassword)) {
-alert("Your passwords don't match. Try again?");
-} else {
-$.post("register.php", {
-name1: name,
-email1: email,
-password1: password
-}, function(data) {
-if (data == 'You have Successfully Registered.....') {
-$("form")[0].reset();
-}
-alert(data);
-});
-}
-});
-});
